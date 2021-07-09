75d3aa46ae07f279fe67f488165c1bcff92186a3
kahramanrasit <34141599+kahramanrasit@users.noreply.github.com>
2021-07-09 12:08:21 +0300

    09.07.2021

--- odev3.sql
+++ odev3.sql
@@ -7,3 +7,12 @@
 
           SELECT country FROM country 
           WHERE country LIKE '_____%n'; -- 5 tane alt tire var
+         
+-- 3. film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+
+          SELECT title FROM film
+          WHERE title ILIKE '%T%T%T%T%';
+-- 4. film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+
+          SELECT * FROM film 
+          WHERE title LIKE 'C%' and length > 90 and rental_rate = 2.99;
